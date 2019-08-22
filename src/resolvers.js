a86c49d87c92c70733d2c2fd0be1936eccf384e9
Israel Moreira <israel.int@gmail.com>
2019-08-22 20:08:29 -0300

    README

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,10 +1,10 @@
 // Example Database
 
 const users = [
-    { id: 1, name: 'Israel', email: 'imoreira@leanwork.com.br'},
-    { id: 2, name: 'Lucas', email: 'loliveira@leanwork.com.br'},
-    { id: 3, name: 'Leo', email: 'leonardo@leanwork.com.br'},
-    { id: 4, name: 'Michel', email: 'michel@leanwork.com.br'},
+    { id: 1, name: 'Israel', email: 'a@lean.com.br'},
+    { id: 2, name: 'Lucas', email: 'b@lean.com.br'},
+    { id: 3, name: 'Leo', email: 'c@lean.com.br'},
+    { id: 4, name: 'Michel', email: 'd@lean.com.br'},
 ]
 
 module.exports = {
